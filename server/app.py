faf3bfad675ab008a966ce19aa71e9e2c9b3ec6f
ricardo <richisanj@gmail.com>
2025-10-25 17:13:41 -0400

    Subiendo proyecto Flask + ESP32

--- server/app.py
+++ server/app.py
@@ -1,39 +1,23 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
 from flask_cors import CORS
-from pymongo import MongoClient
-import os
 
-app = Flask(__name__, static_folder="../web")
+app = Flask(__name__)
 CORS(app)
 
-# Conexión a MongoDB
-client = MongoClient("mongodb+srv://RicardoSanjines:RicardoSanjines@cluster0.rhtbcma.mongodb.net/?retryWrites=true&w=majority")
-db = client["contadorDB"]
-coleccion = db["datos"]
+app.config["MONGO_URI"] = "tu_uri_de_mongodb"
+mongo = PyMongo(app)
 
-# Ruta para recibir datos del ESP32
-@app.route("/contador", methods=["GET", "POST"])
-def contador():
-    if request.method == "POST":
-        data = request.get_json()
-        if "pulsos" in data:
-            coleccion.insert_one(data)
-            return jsonify({"status": "ok"}), 201
-        return jsonify({"error": "Dato incorrecto"}), 400
-    else:
-        datos = list(coleccion.find({}, {"_id": 0}))
-        return jsonify(datos)
+@app.route('/data', methods=['POST'])
+def recibir_dato():
+    data = request.get_json()
+    mongo.db.contador.insert_one(data)
+    return jsonify({"message": "Dato recibido"}), 200
 
-# Servir HTML desde /
-@app.route("/")
-def index():
-    return send_from_directory(app.static_folder, "index.html")
+@app.route('/data', methods=['GET'])
+def obtener_datos():
+    datos = list(mongo.db.contador.find({}, {"_id": 0}))
+    return jsonify(datos)
 
-# Servir JS y CSS
-@app.route("/<path:path>")
-def static_proxy(path):
-    return send_from_directory(app.static_folder, path)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run()
